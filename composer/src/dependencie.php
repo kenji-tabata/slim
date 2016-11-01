a21fe32286ece71564edaf1fe564cae5866786e1
Leandro Kenji <locky.kenji@gmail.com>
2016-11-01 10:12:29 -0200

    Removido

--- composer/src/dependencie.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Get container
-$container = $app->getContainer();
-
-// Register component on container
-$container['view'] = function ($container) {
-    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates/');
-};
